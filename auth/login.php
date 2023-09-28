44203563f385945a0c3df4022f0365d2d0abe282
Huy Panha <huypanha558@gmail.com>
2023-09-29 00:07:07 +0700

    Added Auth

---
+++ auth/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Library Management System</title>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="" method="post">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+            <label for="email">Password</label>
+            <input type="password" name="pass" id="pass" required>
+        </form>
+    </div>
+</body>
+</html>
