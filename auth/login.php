ed7b05f5905b5fac251f642ad396cb5193321f91
Huy Panha <huypanha558@gmail.com>
2023-09-29 00:43:55 +0700

    Login & Style

--- auth/login.php
+++ auth/login.php
@@ -4,15 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Library Management System</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <div class="wrapper">
-        <form action="" method="post">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
-            <label for="email">Password</label>
-            <input type="password" name="pass" id="pass" required>
-        </form>
+        <div class="left-side">
+            <img src="../media/angel-bookstore.jpg" alt="">
+        </div>
+        <div class="right-side">
+            <form action="" method="post">
+                <h1>Welcome Back</h1><br><br>
+                <label for="email">Email</label><br>
+                <input type="email" name="email" id="email" required><br><br><br>
+                <label for="email">Password</label><br>
+                <input type="password" name="pass" id="pass" required><br><br>
+                <div class="align-right">Forgot Password?</div><br><br>
+                <input type="submit" value="Login">
+            </form>
+        </div>
     </div>
 </body>
 </html>
