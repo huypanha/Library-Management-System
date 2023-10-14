2f8b67124b11b0db32a457903bce3f9f7d6ca837
Huy Panha <huypanha558@gmail.com>
2023-10-15 00:27:31 +0700

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'de9915ffd9ea308fa7612b85470fc3a56f431e0d',
+    'name' => 'panha/wbd_sa_final_y3s1',
+  ),
+  'versions' => 
+  array (
+    'panha/wbd_sa_final_y3s1' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'de9915ffd9ea308fa7612b85470fc3a56f431e0d',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.8.1',
+      'version' => '6.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+    ),
+  ),
+);
